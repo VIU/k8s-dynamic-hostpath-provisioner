ae67374cedded1f9ec1f2220aae84ce581e100ee
Sami Salkosuo <samisalkosuo@users.noreply.github.com>
2018-12-05 13:59:22 +0200

    logging and other changes

--- dynamic-hostpath-provisioner.go
+++ dynamic-hostpath-provisioner.go
@@ -24,15 +24,17 @@ and included code from https://github.com/torchbox/k8s-hostpath-provisioner/blob
 package main
 
 import (
-	"errors"
 	"flag"
+	"errors"
 	"os"
 	"path"
 	"syscall"
 	"fmt"
+	"log"
+	"io"
+	"io/ioutil"
 
 	"github.com/kubernetes-sigs/sig-storage-lib-external-provisioner/controller"
-
 	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
@@ -59,20 +61,52 @@ type hostPathProvisioner struct {
  /* Storage the parsed configuration from the storage class */
  type hostPathParameters struct {
 	pvDir       string /* On-disk path of the PV root */
+	enableTrace       bool /* Set to true to enable trace entries */
+}
+
+/*
+Logging configuration. 
+Copied (directly, and without remorse) from: https://www.ardanlabs.com/blog/2013/11/using-log-package-in-go.html
+
+TODO: JSON logging
+https://github.com/sirupsen/logrus
+https://github.com/francoispqt/onelog
+https://github.com/uber-go/zap
+https://github.com/rs/zerolog
+
+*/
+var (
+    Trace   *log.Logger
+    Info    *log.Logger
+    Warning *log.Logger
+    Error   *log.Logger
+)
+
+func InitLogger(
+    traceHandle io.Writer,
+    infoHandle io.Writer,
+    warningHandle io.Writer,
+    errorHandle io.Writer) {
+
+    Trace = log.New(traceHandle,
+        "TRACE: ", log.Lshortfile)
+
+    Info = log.New(infoHandle,
+        "INFO: ", log.Lshortfile)
+
+    Warning = log.New(warningHandle,
+        "WARNING: ", log.Lshortfile)
+
+    Error = log.New(errorHandle,
+        "ERROR: ", log.Lshortfile)
 }
 
 
 // NewHostPathProvisioner creates a new hostpath provisioner
 func NewHostPathProvisioner(client kubernetes.Interface) controller.Provisioner {
 	
-	//No need for this, because it is assumed that directories
-	//to be created are shared between worker nodes.
-	/*
-	nodeName := os.Getenv("NODE_NAME")
-	if nodeName == "" {
-		glog.Fatal("env variable NODE_NAME must be set so that this provisioner can identify itself")
-	}
-	*/
+	Info.Println("Creating NewHostPathProvisioner")
+
 	return &hostPathProvisioner{
 		client:    client,
 		identity: provisionerIDAnn,
@@ -83,6 +117,7 @@ var _ controller.Provisioner = &hostPathProvisioner{}
 
 // Provision creates a storage asset and returns a PV object representing it.
 func (p *hostPathProvisioner) Provision(options controller.VolumeOptions) (*v1.PersistentVolume, error) {
+
 	/*
 	* Fetch the PV root directory from the PV storage class.
 	*/
@@ -91,10 +126,16 @@ func (p *hostPathProvisioner) Provision(options controller.VolumeOptions) (*v1.P
 		return nil, err
 	}
 
+	Trace.Println("Provisioning directory")
+	Trace.Println("Options:",options)
+
 	 /* Create the on-disk directory. */
+	 //TODO: check how to generate PVPath
+	 //https://github.com/nmasse-itix/OpenShift-HostPath-Provisioner/blob/master/src/hostpath-provisioner/hostpath-provisioner.go
 	 path := path.Join(params.pvDir, options.PVName)
 	 if err := os.MkdirAll(path, 0777); err != nil {
-		 fmt.Printf("ERROR: failed to mkdir %s: %s", path, err)
+		 //fmt.Printf("ERROR: failed to mkdir %s: %s", path, err)
+		 Error.Println(fmt.Sprintf("failed to mkdir %s: %s", path, err))
 		 return nil, err
 	 }
 
@@ -119,7 +160,7 @@ func (p *hostPathProvisioner) Provision(options controller.VolumeOptions) (*v1.P
 		},
 	}
 
-	fmt.Printf("successfully created hostpath volume %s (%s)", options.PVName, path)
+	Trace.Println(fmt.Sprintf("successfully created hostpath volume %s (%s)", options.PVName, path))
 
 	return pv, nil
 }
@@ -143,14 +184,13 @@ func (p *hostPathProvisioner) Delete(volume *v1.PersistentVolume) error {
 	class, err := p.client.StorageV1beta1().StorageClasses().Get(v1helper.GetPersistentVolumeClass(volume),
 		metav1.GetOptions{})
 	if err != nil {
-		fmt.Printf("not removing volume <%s>: failed to fetch storageclass: %s",
-			   volume.Name, err)
+		//fmt.Printf("not removing volume <%s>: failed to fetch storageclass: %s",volume.Name, err)
+		Error.Println(fmt.Sprintf("not removing volume <%s>: failed to fetch storageclass: %s",volume.Name, err))
 		return err
 	}
 	params, err := p.parseParameters(class.Parameters)
 	if err != nil {
-		fmt.Printf("not removing volume <%s>: failed to parse storageclass parameters: %s",
-			   volume.Name, err)
+		Error.Println(fmt.Sprintf("not removing volume <%s>: failed to parse storageclass parameters: %s",volume.Name, err))
 		return err
 	}
 
@@ -160,9 +200,8 @@ func (p *hostPathProvisioner) Delete(volume *v1.PersistentVolume) error {
 	 */
 	 path := path.Join(params.pvDir, volume.Name)
 	 if err := os.RemoveAll(path); err != nil {
-		fmt.Printf("ERROR: failed to remove PV %s (%s): %v",
-			 volume.Name, path, err)
-		 return err
+		Error.Println(fmt.Sprintf("not removing volume <%s>: failed to parse storageclass parameters: %s",volume.Name, err))
+		return err
 	 }
  
 	return nil
@@ -173,16 +212,25 @@ func (p *hostPathProvisioner) Delete(volume *v1.PersistentVolume) error {
 */
 func (p *hostPathProvisioner) parseParameters(parameters map[string]string) (*hostPathParameters, error) {
 	var params hostPathParameters
-
+	
 	for k, v := range parameters {
 		switch k {
 		case "pvDir":
 			params.pvDir = v
- 
+			
+		case "enableTrace":
+			if v == "true" {
+				InitLogger(os.Stdout, os.Stdout, os.Stdout, os.Stderr)
+			} else {
+				InitLogger(ioutil.Discard, os.Stdout, os.Stdout, os.Stderr)
+			}		
 		default:
 			return nil, fmt.Errorf("invalid option %q", k)
 		}
 	}
+
+	Trace.Println("storageclass parameters: ",parameters)
+
 
 	if params.pvDir == "" {
 		return nil, fmt.Errorf("missing PV directory (pvDir)")
@@ -191,29 +239,30 @@ func (p *hostPathProvisioner) parseParameters(parameters map[string]string) (*ho
 	return &params, nil
 }
 
-
 func main() {
 	syscall.Umask(0)
-
 	flag.Parse()
 	flag.Set("logtostderr", "true")
+
+	// initialize logger
+	InitLogger(os.Stdout, os.Stdout, os.Stdout, os.Stderr)
 
 	// Create an InClusterConfig and use it to create a client for the controller
 	// to use to communicate with Kubernetes
 	config, err := rest.InClusterConfig()
 	if err != nil {
-		fmt.Printf("ERROR: failed to create config: %v", err)
+		Error.Println(fmt.Sprintf("ERROR: failed to create config: %v", err))
 	}
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		fmt.Printf("ERROR: to create client: %v", err)
+		Error.Println(fmt.Sprintf("ERROR: to create client: %v", err))
 	}
 
 	// The controller needs to know what the server version is because out-of-tree
 	// provisioners aren't officially supported until 1.5
 	serverVersion, err := clientset.Discovery().ServerVersion()
 	if err != nil {
-		fmt.Printf("ERROR: error getting server version: %v", err)
+		Error.Println(fmt.Sprintf("ERROR: error getting server version: %v", err))
 	}
    
 	// Create the provisioner: it implements the Provisioner interface expected by
